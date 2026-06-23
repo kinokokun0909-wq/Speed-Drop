22fff4d5b3b1e60dddb0af5eaa86ef0dd7638d35
Xkoudai <kinokokun0909@gmail.com>
2026-06-23 14:20:20 +0900

    タイトル　設定　メニュー

---
+++ Assets/_Project/Scripts/Core/TitleMenuController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject titlePanel;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject menuPanel;
+
+    [SerializeField] private string gameSceneName = "test Scene";
+
+    private void Start()
+    {
+        ShowTitle();
+    }
+
+    public void ShowTitle()
+    {
+        titlePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        menuPanel.SetActive(false);
+    }
+
+    public void ShowSettings()
+    {
+        titlePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+        menuPanel.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+        titlePanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+
+    public void ShowModeSelect()
+    {
+        titlePanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        menuPanel.SetActive(false);
+    }
+
+    public void StartNormalMode()
+    {
+        PlayerPrefs.SetString("SelectedMode", "Normal");
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
